e9e8915fde51a8f802cafe5e12346cfa399e7bbc
Sachin <sachinbajaj0001@gmail.com>
2023-10-23 11:34:44 +0530

    Finish

--- index.js
+++ index.js
@@ -9,8 +9,20 @@ app.use(cors({origin: 'http://localhost:3000'}));
 const httpServer=createServer(app);
 const io=new Server(httpServer, { cors: 'http://localhost:3000' });
 
-io.on("connection", (Socket) => {
+io.on("connection", (socket) => {
     console.log("server connected");
+
+    socket.on('beginPath', (arg) => {
+        socket.broadcast.emit('beginPath', arg);
+    });
+
+    socket.on('drawLine', (arg) => {
+        socket.broadcast.emit('drawLine', arg);
+    });
+
+    socket.on('changeConfig', (arg) => {
+        socket.broadcast.emit('changeConfig', arg);
+    });
 });
 
 
